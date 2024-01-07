32f4f4fa34879552f9d76978cbbacd16389c2ff1
sangam0207 <127228515+sangam0207@users.noreply.github.com>
2024-01-07 22:09:06 +0530

    Update Data.js
    We can filter the user by his name in this page also..

--- client/src/page/Data.js
+++ client/src/page/Data.js
@@ -1,60 +1,112 @@
-
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 function Data() {
-  const [data,setDaata]=useState([]);
-  
-  
-  useEffect(()=>{
-    const fetchData=async()=>{
-      try{
-          const response=await axios.get("http://localhost:3000/fetchData");
-    setDaata(response.data); 
-      console.log(response.data)     }
-        catch(error){
-          console.log(error);
+  const [data, setData] = useState([]);
+  const [filterName, setFilterName] = useState('');
 
-        }
-    }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/fetchData");
+        setData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
     fetchData();
-  },[])
-  return (
-   <>
-    <Link to="/" style={{width:'100px',backgroundColor:'#3498db',border:'1px solid black', margin:'40px',color:'white',fontWeight:500,textDecoration:'none',padding:'15px'}}>Home page</Link>
-   <Link to='/filter' style={{width:'100px',backgroundColor:'#3498db',border:'1px solid black', margin:'40px',color:'white',fontWeight:500,textDecoration:'none' ,padding:'15px'}}>Filter</Link>
-   
-   <table style={{ width:"100%",borderCollapse:"collapse",marginTop:"20px"}}>
+  }, []);
 
-    <thead style={{ backgroundColor:"#f2f2f2"}}>
-      <tr >
-        <th style={{ padding:"12px",textAlign:"center",border:"2px solid black"}}>id</th>
-        <th style={{ padding:"12px",textAlign:"center",border:"2px solid black"}}>Name</th>
-        <th style={{ padding:"12px",textAlign:"center",border:"2px solid black"}}>UserName</th>
-        <th style={{ padding:"12px",textAlign:"center",border:"2px solid black"}}>Email</th>
-        <th style={{ padding:"12px",textAlign:"center",border:"2px solid black"}}>address</th>
-      </tr>
-      </thead>
-      <tbody>
-        {
-           data.map((item)=>{
-            return(<tr key={item.id} >
-              <td style={{ padding:"12px",textAlign:"center",border:"2px solid black"}}>{item.id}</td>
-              <td style={{ padding:"12px",textAlign:"center",border:"2px solid black"}}>{item.name}</td>
-              <td style={{ padding:"12px",textAlign:"center",border:"2px solid black"}}>{item.username}</td>
-              <td style={{ padding:"12px",textAlign:"center",border:"2px solid black"}}>{item.email}</td>
-              <td style={{ padding:"12px",textAlign:"center",border:"2px solid black"}}>{item.address.city}</td>
-            </tr>)
-            
-          })
+  const handleFilterByName = async (e) => {
+    setFilterName(e.target.value); 
+    try {
+      const response = await axios.post("http://localhost:3000/filterByName", {
+        fullName: e.target.value,
+      }, {
+        headers: {
+          "Content-Type": "application/json"
         }
-      </tbody>
+      });
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-   
-   </table>
-   </>
-   
+  return (
+    <>
+      <Link
+        to="/"
+        style={{
+          width: '100px',
+          backgroundColor: '#3498db',
+          border: '1px solid black',
+          margin: '40px',
+          color: 'white',
+          fontWeight: 500,
+          textDecoration: 'none',
+          padding: '15px',
+        }}
+      >
+        Home page
+      </Link>
+      <Link
+        to="/filter"
+        style={{
+          width: '100px',
+          backgroundColor: '#3498db',
+          border: '1px solid black',
+          margin: '40px',
+          color: 'white',
+          fontWeight: 500,
+          textDecoration: 'none',
+          padding: '15px',
+        }}
+      >
+        Filter
+      </Link>
+
+      <form style={{marginBlock:'30px'}}>
+        <label>
+          Filter by Name:
+          <input
+            type="text"
+            value={filterName}
+            name="searchItem"
+            onChange={handleFilterByName}
+            style={{backgroundColor:'lightBlue',fontSize:'2rem',width:'200px',height:'40px'}}
+          />
+        </label>
+      </form>
+
+      {data.length === 0 ? (
+        <p>No data found</p>
+      ) : (
+        <table style={{ width: '100%', borderCollapse: 'collapse', marginTop: '20px' }}>
+          <thead style={{ backgroundColor: "#f2f2f2" }}>
+            <tr>
+              <th style={{ padding: "12px", textAlign: "center", border: "2px solid black" }}>id</th>
+              <th style={{ padding: "12px", textAlign: "center", border: "2px solid black" }}>Name</th>
+              <th style={{ padding: "12px", textAlign: "center", border: "2px solid black" }}>UserName</th>
+              <th style={{ padding: "12px", textAlign: "center", border: "2px solid black" }}>Email</th>
+              <th style={{ padding: "12px", textAlign: "center", border: "2px solid black" }}>address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id} >
+                <td style={{ padding: "12px", textAlign: "center", border: "2px solid black" }}>{item.id}</td>
+                <td style={{ padding: "12px", textAlign: "center", border: "2px solid black" }}>{item.name}</td>
+                <td style={{ padding: "12px", textAlign: "center", border: "2px solid black" }}>{item.username}</td>
+                <td style={{ padding: "12px", textAlign: "center", border: "2px solid black" }}>{item.email}</td>
+                <td style={{ padding: "12px", textAlign: "center", border: "2px solid black" }}>{item.address.city}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
   );
 }
 
