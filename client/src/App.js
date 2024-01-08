28793d0addbb54ed569d989c6bd3dd666fb9da67
sangam0207 <127228515+sangam0207@users.noreply.github.com>
2024-01-08 23:04:25 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Data from './page/Data'
 import SearchForm from './page/SearchData'
 import {Route,Routes} from 'react-router-dom'
 import FilterData from './page/FilterData'
+import QueryData from './page/QueryData'
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element={<SearchForm/>}/>
       <Route path='/allData' element={<Data/>}/>
       <Route path='/filter' element={<FilterData/>}/>
+      <Route path='/byQuery' element={<QueryData/>}/>
       </Routes> 
     </>
   )
